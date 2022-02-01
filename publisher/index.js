c378b0973b01740c294c7e3de7ccb0b67bb0f59e
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2022-02-01 18:49:27 +0300

    publisher

---
+++ publisher/index.js
@@ -0,0 +1,25 @@
+var amqplib = require('amqplib')
+
+var amqpConnection = amqplib.connect('amqp://localhost');   // инстанс подключения
+
+const queue = 'dev-queue';    // название очереди
+
+const message = "something to do";
+const messageBytes = Buffer.from(message);
+
+// Publisher
+amqpConnection
+    .then(function(connection) {
+        return connection.createChannel();    // подключение возвращает канал
+    })
+    .then(function(channel) {
+        return channel.assertQueue(queue, {
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        }).then(function(ok) {
+                return channel.sendToQueue(queue, messageBytes);    // отправка в очередь
+            });
+    })
+    .catch(console.warn);
